464dd2b46ccd0574408b5533ab5567d7286f1483
Santino Degra <santinodegra73@gmail.com>
2022-07-10 20:27:09 -0300

    first

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const myconnection = require('express-myconnection');
+const { create } = require('express-handlebars');
+const session = require('express-session');
+const mysql = require('mysql');
+const app = express();
+require('dotenv').config();
+
+app.listen();
