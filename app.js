16b946ce130698af7015a31a88ed1506c5248bf0
Santino Degra <santinodegra73@gmail.com>
2022-07-10 20:35:26 -0300

    1

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const mysql = require('mysql');
 const app = express();
 require('dotenv').config();
 
-app.listen();
+app.get('/',(req,res)=>{
+    res.send('Hola')
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT,()=>console.log('up!🤩'));
